698e7e4295e0c26056ba461ed23cab92848933df
rure00 <asnjkd@naver.com>
2024-11-10 17:47:37 +0900

    add: hiltApplication and bindModule

---
+++ app/src/main/java/com/example/internproject/data/di/BindModule.kt
@@ -0,0 +1,17 @@
+package com.example.internproject.data.di
+
+import com.example.internproject.data.repository.UserRepositoryImpl
+import com.example.internproject.domain.repository.UserRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+
+@Module
+@InstallIn(ViewModelComponent::class)
+abstract class BindModule {
+    @Binds
+    @ViewModelScoped
+    abstract fun provideUserRepository(impl: UserRepositoryImpl): UserRepository
+}
