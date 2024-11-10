f95ea5cc5542272245151dfb2dc86f1711d7805d
rure00 <asnjkd@naver.com>
2024-11-10 17:52:11 +0900

    finish: userRepositoryImpl

--- app/src/main/java/com/example/internproject/data/datasource/UserDataSource.kt
+++ app/src/main/java/com/example/internproject/data/datasource/UserDataSource.kt
@@ -1,8 +1,22 @@
 package com.example.internproject.data.datasource
 
+import com.example.internproject.data.dto.SignUpDto
+import com.example.internproject.domain.results.DuplicatedResult
+import com.example.internproject.domain.results.SignUpResult
 import javax.inject.Inject
 
 class UserDataSource @Inject constructor(
 
 ) {
+    suspend fun trySignUp(signUpDto: SignUpDto): SignUpResult {
+
+    }
+
+    suspend fun checkName(name: String): DuplicatedResult {
+
+    }
+
+    suspend fun checkId(id: String): DuplicatedResult {
+
+    }
 }
