5594ed5eca178fa61388633924020c626ccfb739
rure00 <asnjkd@naver.com>
2025-01-19 18:12:49 +0900

    add: picker

---
+++ app/src/main/java/com/example/internproject/presentation/compose/component/NumberPicker.kt
@@ -0,0 +1,133 @@
+package com.example.internproject.presentation.compose.component
+
+import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Gray
+import androidx.compose.ui.graphics.CompositingStrategy
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.TextStyle
+import com.example.internproject.ui.theme.Black
+import com.example.internproject.ui.theme.Typography
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+
+@Composable
+fun NumberPicker(
+    items: List<Int>,
+    defaultState: Int,
+    modifier: Modifier = Modifier,
+    visibleItemsCount: Int = 3,
+    textModifier: Modifier = Modifier,
+    textStyle: TextStyle = Typography.bodyMedium,
+    selectedColor: Color = Black,
+    unselectedColor: Color = Gray,
+    dividerColor: Color = Black,
+    onItemChanged: (Int) -> Unit,
+) {
+    val startIndex = items.indexOf(defaultState)
+
+    val visibleItemsMiddle = visibleItemsCount / 2
+    val listScrollCount = Integer.MAX_VALUE
+    val listScrollMiddle = listScrollCount / 2
+    val listStartIndex = listScrollMiddle - listScrollMiddle % items.size - visibleItemsMiddle + startIndex
+
+    val selectedItem = remember { mutableStateOf(defaultState) }
+    val selectedItemIndex = remember { mutableStateOf(listStartIndex) }
+
+
+    fun getItem(index: Int) = items[index % items.size]
+
+    val listState = remember(items.size) { LazyListState(firstVisibleItemIndex = listStartIndex) }
+
+    val flingBehavior = rememberSnapFlingBehavior(lazyListState = listState)
+
+    val itemHeightPixels = remember { mutableStateOf(0) }
+    val itemHeightDp = pixelsToDp(itemHeightPixels.value)
+
+    val fadingEdgeGradient = remember {
+        Brush.verticalGradient(
+            0f to Color.Transparent,
+            0.5f to Color.Black,
+            1f to Color.Transparent
+        )
+    }
+
+    LaunchedEffect(listState, items.size) {
+        snapshotFlow { listState.firstVisibleItemIndex }
+            .map { index -> getItem(index + visibleItemsMiddle) }
+            .distinctUntilChanged()
+            .collect { item ->
+                selectedItemIndex.value = listState.firstVisibleItemIndex + visibleItemsCount/2
+                selectedItem.value = item
+                onItemChanged(selectedItem.value)
+            }
+    }
+
+    Box(modifier = modifier) {
+
+        LazyColumn(
+            state = listState,
+            flingBehavior = flingBehavior,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(itemHeightDp * visibleItemsCount)
+                .fadingEdge(fadingEdgeGradient)
+        ) {
+            items(listScrollCount) { index ->
+                Text(
+                    text = getItem(index).toString(),
+                    maxLines = 1,
+                    //overflow = TextOverflow.Ellipsis,
+                    style = textStyle,
+                    modifier = Modifier
+                        .onSizeChanged { size -> itemHeightPixels.value = size.height }
+                        .then(textModifier),
+                    color = if(index == selectedItemIndex.value) selectedColor
+                    else unselectedColor
+                )
+            }
+        }
+
+        HorizontalDivider(
+            modifier = Modifier.offset(y = itemHeightDp * visibleItemsMiddle),
+            color = dividerColor
+        )
+
+        HorizontalDivider(
+            modifier = Modifier.offset(y = itemHeightDp * (visibleItemsMiddle + 1)),
+            color = dividerColor
+        )
+
+    }
+}
+
+private fun Modifier.fadingEdge(brush: Brush) = this
+    .graphicsLayer(compositingStrategy = CompositingStrategy.Offscreen)
+    .drawWithContent {
+        drawContent()
+        drawRect(brush = brush, blendMode = BlendMode.DstIn)
+    }
+
+@Composable
+private fun pixelsToDp(pixels: Int) = with(LocalDensity.current) { pixels.toDp() }
---
+++ app/src/main/java/com/example/internproject/presentation/compose/screen/page/WriteBirthPage.kt
@@ -0,0 +1,91 @@
+package com.example.internproject.presentation.compose.screen.page
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color.Companion.LightGray
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.internproject.R
+import com.example.internproject.presentation.ui_state.ResultUiState
+import com.example.internproject.presentation.utils.SignUpField
+import com.example.internproject.presentation.utils.ValidateSignUp
+import com.example.internproject.presentation.viewmodels.SignUpViewModel
+import com.example.internproject.ui.theme.Black
+import com.example.internproject.ui.theme.TossBlue
+import com.example.internproject.ui.theme.Typography
+import com.example.internproject.ui.theme.White
+import java.time.LocalDate
+
+@Composable
+fun WriteBirthPage(
+    onBackButton: () -> Unit,
+    onNext: (LocalDate) -> Unit,
+    signUpViewModel: SignUpViewModel = hiltViewModel<SignUpViewModel>()
+) {
+    val birthState = remember { mutableStateOf(LocalDate.now()) }
+    val activateNextButton = remember { mutableStateOf(false) }
+
+    val warningMsg = remember { mutableStateOf("") }
+    val checkDuplicated = signUpViewModel.duplicatingUiState.collectAsState()
+    when(checkDuplicated.value) {
+        is ResultUiState.Success -> {
+            onNext(birthState.value)
+        }
+        is ResultUiState.Failure -> {
+            activateNextButton.value = false
+        }
+        else -> {
+
+        }
+    }
+
+    Column(modifier = Modifier) {
+        Image(
+            painter = painterResource(R.drawable.back_btn),
+            contentDescription = null,
+            modifier = Modifier.size(40.dp).padding(start = 10.dp).clickable {
+                onBackButton()
+            }
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Column(modifier = Modifier.padding(horizontal = 10.dp).weight(1f)) {
+            Text(
+                text = "생년월일을 입력해주세요.",
+                style = Typography.titleLarge,
+                color = Black
+            )
+
+            Spacer(modifier = Modifier.height(40.dp).fillMaxWidth())
+
+
+        }
+    }
+}
